2d39266430fb48a0a04b27b55ebf4108bebac5bf
SandlerJr <sandlerjunin@gmail.com>
2023-11-28 08:29:57 -0300

    melhorando o codigo do Desafio 16

--- Desafio16/media_das_turmas.js
+++ Desafio16/media_das_turmas.js
@@ -3,11 +3,10 @@ const salaJava = [6, 5, 8, 9, 5, 6 ];
 const salaPython = [7, 3.5, 8, 9.5];
 
 function calculaMedia(notasDaSala){
-    const somaDasNotas = notasDaSala.reduce((acumulador, nota) => {
-        return acumulador + nota;
-    }, 0);
+    const somaDasNotas = notasDaSala.reduce((acumulador, nota) => acumulador + nota, 0);
 
     media = somaDasNotas / notasDaSala.length;
+    
     return media;
 }
 
