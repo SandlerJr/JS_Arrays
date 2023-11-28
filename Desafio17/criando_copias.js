b999db8f326232ec6a33e3483875edf54f44f0d3
SandlerJr <sandlerjunin@gmail.com>
2023-11-28 08:50:51 -0300

    Desafio 17: criando  copias de arrays

---
+++ Desafio17/criando_copias.js
@@ -0,0 +1,8 @@
+const notas = [7, 7, 8, 9];
+
+const novasNotas = [notas];
+
+novasNotas.push(8);
+
+console.log(`As novas notas da turma são: ${novasNotas}`);
+console.log(`As antigas notas da turma são: ${notas}`);
